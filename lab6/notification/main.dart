f63fb8c7ef757aa5224634590dbb5cad7f6b6fe9
bubupro9x <33770357+bubupro9x@users.noreply.github.com>
2022-10-13 17:10:10 +0700

    Update main.dart

--- lab6/notification/main.dart
+++ lab6/notification/main.dart
@@ -3,11 +3,18 @@ import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
 
+  print("Handling a background message: ${message.messageId}");
+}
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
 
@@ -59,7 +66,17 @@ class _MyHomePageState extends State<MyHomePage> {
     print('User granted permission: ${settings.authorizationStatus}');
     String? token = await messaging.getToken();
     print('token fcm: $token');
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
   }
+
 
   int _counter = 0;
 
